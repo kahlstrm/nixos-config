6158e22d56fe221de7e856bd843fc2026b7232de
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-01-02 18:13:11 +0200

    add customized title to nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -142,6 +142,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Change terminal title to include stuff
+vim.opt.title = true
+vim.opt.titlestring = 'nvim %{fnamemodify(getcwd(),":t")}'
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
