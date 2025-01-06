3e53f3695845a17a98fde8a989ec19a441fc1c17
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-01-07 00:05:20 +0200

    nvim disable treesitter on big files

--- config/nvim/lua/kahlstrm/plugins/treesitter.lua
+++ config/nvim/lua/kahlstrm/plugins/treesitter.lua
@@ -15,6 +15,14 @@ return {
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            vim.notify('File larger than 100KB treesitter disabled for performance', vim.log.levels.WARN, { title = 'Treesitter' })
+            return true
+          end
+        end,
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
