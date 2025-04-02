4fc8be105400529cf96055248cbf188abc365a8c
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-04-02 23:45:18 +0300

    remove declarative setting of timezone

--- machines/frame-work.nix
+++ machines/frame-work.nix
@@ -30,9 +30,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Helsinki";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
