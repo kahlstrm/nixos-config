402a80660d38ea337aec2f4b7ca6c752867e0dfe
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-04-12 16:20:23 +0300

    disable intune for now

--- machines/frame-work.nix
+++ machines/frame-work.nix
@@ -110,7 +110,7 @@
       #  thunderbird
     ];
   };
-  services.intune.enable = true;
+  # services.intune.enable = true;
   # environment.systemPackages = with pkgs; [ microsoft-edge ];
 
   # Install firefox.
