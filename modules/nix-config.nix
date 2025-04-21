4d206286f7c772f0a4e3e8524b1af57fd1209469
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-04-21 22:54:19 +0300

    add missing cachix public key

--- modules/nix-config.nix
+++ modules/nix-config.nix
@@ -16,7 +16,10 @@
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
       ];
-      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
       experimental-features = [
         "nix-command"
         "flakes"
