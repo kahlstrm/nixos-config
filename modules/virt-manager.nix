8015200e0c3cc7bf4ae4d226f9ee7dbf0cbfd2e6
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-05-04 06:16:28 +0300

    add onBoot ignore to libvirtd

--- modules/virt-manager.nix
+++ modules/virt-manager.nix
@@ -8,10 +8,13 @@
   # https://nixos.wiki/wiki/Virt-manager
   programs.virt-manager.enable = true;
   users.groups.libvirtd.members = [ currentSystemUser ];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.libvirtd.qemu = {
-    swtpm.enable = true;
-    ovmf.packages = [ pkgs.OVMFFull.fd ];
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
+    };
+    onBoot = "ignore";
   };
   virtualisation.spiceUSBRedirection.enable = spiceUSBRedirectionEnabled;
 }
