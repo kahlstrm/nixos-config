3054bbd38f49760d223f0f408d477eadcd71f8d5
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-05-16 19:55:53 +0200

    remove default values being specified twice in nix settings

--- modules/nix-config/default.nix
+++ modules/nix-config/default.nix
@@ -18,17 +18,15 @@ in
     settings = {
       allowed-users = [ "${currentSystemUser}" ];
       trusted-users = [
-        "root"
         "${currentSystemUser}"
       ];
 
       substituters = [
         "https://nix-community.cachix.org"
-        "https://cache.nixos.org"
       ];
+
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
       experimental-features = [
         "nix-command"
