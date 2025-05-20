7cf9639d208dad6daf782be61ad0d49ba17e0e80
Kalle Ahlström <71292737+kahlstrm@users.noreply.github.com>
2025-05-20 16:53:42 +0200

    Fix eslint fix all on write

--- config/nvim/lua/kahlstrm/plugins/lsp.lua
+++ config/nvim/lua/kahlstrm/plugins/lsp.lua
@@ -218,7 +218,7 @@ return {
           on_attach = function(client, bufnr)
             vim.api.nvim_create_autocmd('BufWritePre', {
               buffer = bufnr,
-              command = 'EslintFixAll',
+              command = 'LspEslintFixAll',
             })
           end,
         },
