08759ae7bf31816ff89e597017cf7eae4619b124
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-05-24 18:38:26 +0300

    detect nerdfont based on TERM

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,7 +9,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font = os.getenv 'TERM' == 'xterm-ghostty'
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
