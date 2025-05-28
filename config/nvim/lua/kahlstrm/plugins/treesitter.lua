3d94639b75dab91e5f11a6937c0553e7d2695514
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-05-28 09:25:11 +0300

    Revert "update lazy move nvim-treesitter to main", bad idea
    This reverts commit e9283c5e3141e0e104fc9ee78507d3de7f97621c.

--- config/nvim/lua/kahlstrm/plugins/treesitter.lua
+++ config/nvim/lua/kahlstrm/plugins/treesitter.lua
@@ -2,9 +2,8 @@ return {
   -- https://github.com/nvim-treesitter/nvim-treesitter
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    branch = 'main',
-    lazy = false,
     build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
