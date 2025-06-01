0297936bfed3e47ff6ab9ae7c691a3767915963e
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-06-01 22:26:55 +0300

    remove now redundant openjfx11

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,10 +3,7 @@
   other ".nix" files in this directory are automatically loaded.
 */
 final: prev: {
-  intune-portal = final.callPackage ../pkgs/intune-portal.nix { };
+  # intune-portal = final.callPackage ../pkgs/intune-portal.nix { };
   # required for microsoft-identity-broker
-  openjfx11 = final.callPackage ../pkgs/openjfx11/default.nix {
-    withWebKit = true;
-  };
   microsoft-identity-broker = final.callPackage ../pkgs/microsoft-identity-broker.nix { };
 }
