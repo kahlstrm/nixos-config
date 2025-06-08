dc5e7b8ee20cd940b71219da9e6fc6fa2334fec8
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-06-08 16:24:04 +0300

    nix flake update

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,7 +3,5 @@
   other ".nix" files in this directory are automatically loaded.
 */
 final: prev: {
-  intune-portal = final.callPackage ../pkgs/intune-portal.nix { };
-  # required for microsoft-identity-broker
   microsoft-identity-broker = final.callPackage ../pkgs/microsoft-identity-broker.nix { };
 }
