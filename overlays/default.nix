ad623b5c5727dce6495a91f1ed01a9087d1de909
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-06-27 21:23:14 +0300

    fix amdgpu on linux temporarily

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,4 +5,15 @@
 final: prev: {
   # this is how one would e.g. overlay the `microsoft-identity-broker` package with new package definition in ../pkgs/microsoft-identity-broker.nix
   # microsoft-identity-broker = final.callPackage ../pkgs/microsoft-identity-broker.nix { };
+  # temporary fix for amdgpu
+  linux-firmware = prev.linux-firmware.overrideAttrs (old: rec {
+    pname = "linux-firmware";
+    version = "20250624";
+    src = prev.fetchFromGitLab {
+      owner = "kernel-firmware";
+      repo = "linux-firmware";
+      rev = "b05fabcd6f2a16d50b5f86c389dde7a33f00bb81";
+      hash = "sha256-AvSsyfKP57Uhb3qMrf6PpNHKbXhD9IvFT1kcz5J7khM=";
+    };
+  });
 }
