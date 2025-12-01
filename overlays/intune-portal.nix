d1955f44807a93da325dc409befa9c0cae990f74
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-12-02 01:34:22 +0200

    intune-portal: update to 1.2511.7-noble

--- overlays/intune-portal.nix
+++ overlays/intune-portal.nix
@@ -1,11 +1,9 @@
-final: prev:
-{
-  # Override intune-portal to test 1.2508.17 (reports of 1.2511.7 crashes)
+final: prev: {
   intune-portal = prev.intune-portal.overrideAttrs (old: rec {
-    version = "1.2508.17-noble";
+    version = "1.2511.7-noble";
     src = prev.fetchurl {
       url = "https://packages.microsoft.com/ubuntu/24.04/prod/pool/main/i/intune-portal/intune-portal_${version}_amd64.deb";
-      hash = "sha256-UTP+Z6xsjr48deizuwVDb8GrpeeAf5RZwloXsZ7Um3E=";
+      hash = "sha256-MHvAmkemx28ZNcVloFNxJ03YbxrgVPvB7OOMYR6Oyo8=";
     };
   });
 }
