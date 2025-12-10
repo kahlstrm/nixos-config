ff4fc326978d6c75d4f08ddf062c2fecd0466b39
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-12-10 22:23:35 +0200

    nvim add glsl lsp

--- config/nvim/lua/kahlstrm/plugins/lsp.lua
+++ config/nvim/lua/kahlstrm/plugins/lsp.lua
@@ -204,6 +204,7 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       ---@type table<string, vim.lsp.Config>
       local servers = {
+        glsl_analyzer = {},
         gopls = {},
         golangci_lint_ls = {},
         pyright = {},
